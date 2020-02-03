0427e0bc85518d786d1f60877fa6b54badda1525
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-03 17:46:19 -0500

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,14 +1,15 @@
 -----------------------------------------------------------------------------------------------------
 
-/* How to create stored procedure */
+/* How to create stored procedure with two joins and two parameters */
 
 USE AdventureWorks2017
 GO
 
 CREATE PROCEDURE dbo.uspGetAddress @City nvarchar(30) = NULL, @AddressLine1 nvarchar(60) = NULL
 AS
-SELECT *
-FROM Person.Address
+SELECT Person.Person.FirstName, Person.Person.MiddleName, Person.Person.LastName, Person.Address.AddressLine1, Person.Address.City
+FROM Person.Address join Person.BusinessEntity ON Person.Address.AddressID = Person.BusinessEntity.BusinessEntityID
+join Person.Person ON Person.BusinessEntity.BusinessEntityID = Person.Person.BusinessEntityID
 WHERE City = ISNULL(@City,City)
 AND AddressLine1 LIKE '%' + ISNULL(@AddressLine1 ,AddressLine1) + '%'
 GO
@@ -20,4 +21,3 @@ GO
 EXEC dbo.uspGetAddress @City = 'Calgary', @AddressLine1 = 'A'
 
 -----------------------------------------------------------------------------------------------------
-SELECT * FROM Person.AddressType
