81558c8aa95e0c82a8f3edcd3b4c16f3c55bbf47
Miles <59800778+MilesHoover@users.noreply.github.com>
2020-02-03 16:58:28 -0500

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,68 +1,23 @@
-----------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 
-CREATE DATABASE MyDatabase2
+/* How to create stored procedure */
 
-----------------------------------------------------------
+USE AdventureWorks2017
+GO
 
-USE MyDatabase2
-
-----------------------------------------------------------
-
-CREATE TABLE Orders (
-	OrderID int IDENTITY(1,1),
-	CarID int,
-	FirstName varchar(255),
-	LastName varchar(255),
-);
-
-----------------------------------------------------------
-
-CREATE TABLE Cars (
-	OrderID int IDENTITY(100,1),
-	CarID int,
-	Make varchar(255),
-	Model varchar(255),
-	Year int
-);
-
-----------------------------------------------------------
-
-INSERT INTO Orders (CarID, FirstName, LastName)
-VALUES 
-	(1000, 'Mike', 'Jones'),
-	(1001, 'Shelby', 'Hacker'),
-	(1002, 'Thomas', 'whicker');
-
-----------------------------------------------------------
-
-INSERT INTO Cars (CarID, Make, Model, Year)
-VALUES
-	(6349645, 'Toyota', '4Runner', 1999),
-	(3876244, 'Nissan', 'Altima', 2007),
-	(0970701, 'Chevrolet', 'Camaro', 2013);
-
-----------------------------------------------------------
-
-SELECT Orders.OrderID, Orders.CarID, Orders.FirstName, Orders.LastName, Cars.Make, Cars.Model
-FROM Orders
-INNER JOIN Cars ON Orders.OrderID=Cars.OrderID;
-GO;
-
-----------------------------------------------------------
-
-CREATE PROCEDURE GetCars @Make nvarchar(30), @Year int
+CREATE PROCEDURE dbo.uspGetAddress @City nvarchar(30) = NULL, @AddressLine1 nvarchar(60) = NULL
 AS
-SELECT * 
-FROM Cars
-WHERE Make = @Make AND Year = @Year
+SELECT *
+FROM Person.Address
+WHERE City = ISNULL(@City,City)
+AND AddressLine1 LIKE '%' + ISNULL(@AddressLine1 ,AddressLine1) + '%'
+GO
 
-----------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 
-EXEC GetCars @Make = 'Toyota', @Year = 1999;
+/* How to call */
 
-----------------------------------------------------------
+EXEC dbo.uspGetAddress @City = 'Calgary', @AddressLine1 = 'A'
 
-SELECT * FROM Orders
-SELECT * FROM Cars
-
-----------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+SELECT * FROM Person.AddressType
